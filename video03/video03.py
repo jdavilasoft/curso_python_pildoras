a1b8b0b6be3288cd08eb2acfac4595754dbe9350
jdavilasoft <jdavila.soft@gmail.com>
2022-11-10 09:48:26 -0500

    Primer commit

---
+++ video03/video03.py
@@ -0,0 +1,30 @@
+'''
+Created on 9 ago. 2019
+
+@author: javierdt
+'''
+
+# Los comentarios van con el simbolo #
+
+# Para mostrar texto, usamos comillas dobles
+print("Hola mundo cruel")
+
+# También podemos usar comilla simples
+print('Adios mundo cruel')
+
+# Podemos poner dos instrucciones seguidas separadas por (;)
+print("Hola mundo cruel"); print('Adios mundo cruel')
+
+# Ejemplo de variable
+mi_nombre = "Hola, mi nombre es Javier"
+print(mi_nombre)
+
+# Se puede separar el texto en varias lineas usando el backslash (\)
+nombre = "Este es una linea de texto muy larga, tan larga que tengo que hacer \
+mas de una linea para que se muestre por completo, sin embargo el usuario no \
+notara la diferencia, xd"
+print(nombre)
+
+# ** identación** Por ahora no sabemos que hace lo siguiente:
+for i in range(5):
+    print(i + 1)
---
+++ video03/video031.py
@@ -0,0 +1,22 @@
+'''
+Created on 3 oct. 2020
+
+@author: javierdt
+'''
+
+print("Hola mundo cruel")
+print('Adios mundo cruel')
+
+print()
+
+print("Hola mundo cruel"); print('Adios mundo cruel')
+
+print()
+
+var = "Es una variable"
+print(var)
+
+print()
+
+for i in range(5):
+    print(i+1)
