dfe8b052e8bbd47b718c673dc3864d00a9be0e2e
jdavilasoft <jdavila.soft@gmail.com>
2022-11-10 15:22:13 -0500

    Video 26

---
+++ video26 - POO/video26_1.py
@@ -0,0 +1,23 @@
+class Coche():
+    largoChasis = 250
+    anchoChasis = 120
+    ruedas = 4
+    enMarcha = False
+
+    def arrancar(self):
+        self.enMarcha = True
+
+    def estado(self):
+        if self.enMarcha:
+            return "El coche esta en marcha"
+        else:
+            return "El coche esta detenido"
+
+miCoche = Coche()
+
+print("El largo del coche es:", miCoche.largoChasis)
+print("El coche tiene:", miCoche.ruedas, "ruedas")
+
+miCoche.arrancar()
+
+print(miCoche.estado())
